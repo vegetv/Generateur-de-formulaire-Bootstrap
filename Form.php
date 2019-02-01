67ab25478ae3bb99bb5d665dc1124363908c6009
Yebas Louis Marie <42536024+vegetv@users.noreply.github.com>
2019-02-01 17:21:25 +0100

    Create Form.php

---
+++ Form.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tutoriel\HTML;
+
+
+/** permet de generer un formulaire simplement
+* class Form
+*/
+class Form{
+	/*
+	* @var array utiliser par le formulaire
+	*/
+	private $data;
+	/*
+	* @var string utiliser pour entourer les champs
+	*/
+	public $surround ='p';
+
+	/*
+	* @param array $data utilisé par le formulaire
+	*/
+
+	public function __construct($data = array()){
+
+		$this->data = $data ;
+	}
+
+	/*
+	* @param $html code html à entourer par surround
+	* @return string
+	*/
+
+	protected function surround($html){
+
+		return "<$this->surround>{$html}</$this->surround>";
+	}
+
+	/*
+	*@param $index string de la valeur à recuperer
+	*@return string
+	*/
+
+	protected function getValue($index){
+
+		if (isset($this->data[$index])) {
+			
+			return $this->data[$index];
+		}
+
+		else{
+
+			return null;
+		}
+	}
+
+	/*
+	*@param $name string qui remplit initialement le formulaire
+	*@return string
+	*/
+
+	public function input ($name){
+
+		return $this->surround('<input type="text" name="'.$name.'" value="'.$this->getValue($name).'">');
+	}
+
+	/*
+	*@return string
+	*/
+	
+	public function submit(){
+
+		return $this->surround('<button type="submit">Envoyer</button>');
+	}
+
+	
+}
